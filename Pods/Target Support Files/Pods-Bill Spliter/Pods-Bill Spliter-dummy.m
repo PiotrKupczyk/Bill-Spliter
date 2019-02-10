c593e40f4dcb0b79569b6dec7b58b3df94763b17
Piotr Kupczyk <piotr.kupczyk97@gmail.com>
2019-02-10 14:03:54 +0100

    init

---
+++ Pods/Target Support Files/Pods-Bill Spliter/Pods-Bill Spliter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bill_Spliter : NSObject
+@end
+@implementation PodsDummy_Pods_Bill_Spliter
+@end
---
+++ Pods/Target Support Files/Pods-Bill SpliterTests/Pods-Bill SpliterTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bill_SpliterTests : NSObject
+@end
+@implementation PodsDummy_Pods_Bill_SpliterTests
+@end
