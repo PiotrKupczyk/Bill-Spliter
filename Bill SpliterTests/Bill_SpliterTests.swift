cf5e698b2ecbf868c7268eec4a7f79ff7ab15cd8
Piotr Kupczyk <piotr.kupczyk97@gmail.com>
2019-02-10 13:37:48 +0100

    Initial Commit

---
+++ Bill SpliterTests/Bill_SpliterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bill_SpliterTests.swift
+//  Bill SpliterTests
+//
+//  Created by Piotr Kupczyk on 10/02/2019.
+//  Copyright © 2019 Piotr Kupczyk. All rights reserved.
+//
+
+import XCTest
+@testable import Bill_Spliter
+
+class Bill_SpliterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
